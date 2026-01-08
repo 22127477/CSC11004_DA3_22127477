749aa5ae75674600ababd61574d9517f794146c6
22127477 <geminiultra97@gmail.com>
2026-01-08 18:44:45 +0700

    Test Webhook Trigger

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Jenkins CI/CD Pipeline! MSSV: 22127477"
+    return "Hello from Jenkins CI/CD Pipeline! MSSV: 22127477asdf"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
